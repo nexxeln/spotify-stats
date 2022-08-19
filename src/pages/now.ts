7f34cdc5729d76c246e4c243852cbafd1013db45
Shoubhit Dash <shoubhit2005@gmail.com>
2022-08-19 13:11:37 +0530

    add now playing endpoint

---
+++ src/pages/now.ts
@@ -0,0 +1,25 @@
+import { nowPlaying, SpotifyArtist } from "../lib/spotify";
+
+export const get = async () => {
+  const response = await nowPlaying();
+
+  if (response.status === 204 || response.status > 400) {
+    return new Response(JSON.stringify({ isPlaying: false }), {
+      status: 200,
+    });
+  }
+
+  const { item } = await response.json();
+
+  const track = {
+    isPlaying: true,
+    title: item.name,
+    artist: item.artists
+      .map((_artist: SpotifyArtist) => _artist.name)
+      .join(", "),
+    url: item.external_urls.spotify,
+    img: item.album.images[0].url,
+  };
+
+  return new Response(JSON.stringify({ track }));
+};
