a3a2bdccebaed905fcdd27792845dbe34033e704
Shoubhit Dash <shoubhit2005@gmail.com>
2022-08-19 20:03:52 +0530

    add env to make absolute url

--- src/env.d.ts
+++ src/env.d.ts
@@ -4,6 +4,7 @@ interface ImportMetaEnv {
   readonly SPOTIFY_CLIENT_ID: string;
   readonly SPOTIFY_CLIENT_SECRET: string;
   readonly SPOTIFY_REFRESH_TOKEN: string;
+  readonly URL: string;
 }
 
 interface ImportMeta {
